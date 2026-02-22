effc687d73f4671ddec2e5c0e11aee96b0f5e3e9
pachara <pachared.amr12@gmail.com>
2026-02-23 01:09:27 +0700

    Update page.tsx

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,4 +1,10 @@
-import FeaturesPage from "@/components/pages/FeaturesPage";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const FeaturesPage = dynamic(() => import("@/components/pages/FeaturesPage"), {
+    ssr: false,
+});
 
 export default function Page() {
     return <FeaturesPage />;
